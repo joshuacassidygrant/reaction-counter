deba6717e261baca282ff34bff931883cacc091c
Joshua Grant <jc.grant22@gmail.com>
2020-08-18 12:03:40 -0700

    refactor

--- routes/reactions.js
+++ routes/reactions.js
@@ -13,74 +13,98 @@ router.get('/:emoji', function(req, res, next) {
   .then((val) => {
     res.send(val);
   })
+  .catch((err) => {
+    console.error(err);
+  })
 });
 
-async function getReactionsOfEmojiByPostAuthor(emoji) {
-  authorToEmojiCount = {};
+function getReactionsOfEmojiByPostAuthor(emoji) {
+  return fetchHistory()
+  .then((messages) => {
+    authorToEmojiCount = {};
+
+    for (let message of messages) {
+      // check if message has any reactions
+      if (!("reactions" in message)) continue;
   
-  let messages = await fetchHistory();
-  for (let message of messages) {
-    
-    // check if message has any reactions
-    if (!("reactions" in message)) continue;
-
-    // find index of given emoji
-    let emojiIndex = message.reactions.findIndex(reaction => reaction.name == emoji);
-    if (emojiIndex == -1) continue;
-
-    // attribute reactions to post author
-    if (!(message.user in authorToEmojiCount)) {
-      authorToEmojiCount[message.user] = 0;
+      // find index of given emoji
+      let emojiIndex = message.reactions.findIndex(reaction => reaction.name == emoji);
+      if (emojiIndex == -1) continue;
+  
+      // attribute reactions to post author
+      if (!(message.user in authorToEmojiCount)) {
+        authorToEmojiCount[message.user] = 0;
+      }
+      authorToEmojiCount[message.user] += message.reactions[emojiIndex].count;
+  
     }
-    authorToEmojiCount[message.user] += message.reactions[emojiIndex].count;
+    return authorToEmojiCount;
+  })
+  .then((authorToEmojiCount) => {
+    // replaces ids with names
+    return getListWithNames(authorToEmojiCount);
+  })
+  .then((scoreList) => {
+    // sorts reactions descending
+    return scoreList.sort((a, b) =>  b.score - a.score);
+  })
 
-  }
-
-  let scoreList = await getListWithNames(authorToEmojiCount);
-  scoreList = scoreList.sort((a, b) =>  b.score - a.score);
-  return scoreList;
 }
-
 
 async function getListWithNames(userIdToReactCount) {
-  scoreList = [];
+  promiseList = [];
 
+  // creates a promise array for all user requests
   for (let userId of Object.keys(userIdToReactCount)) {
-    let userRes = await bolt.client.users.info({
+    promiseList.push(bolt.client.users.info({
       token: process.env.SLACK_BOT_TOKEN,
       user: userId
-    })
-    scoreList.push({name: userRes.user.name, score: userIdToReactCount[userId]});
+    }));
   }
 
-  return scoreList;
+  // once all promises are resolved, constructs a list of {name, score} objects
+  return Promise.all(promiseList)
+  .then ((userList) => {
+    scoreList = [];
+
+    for (userRes of userList) {
+      scoreList.push({name: userRes.user.profile.real_name, score: userIdToReactCount[userRes.user.id]});
+    }
+    
+    // build score list
+    return scoreList;
+  })
+  .catch ((error) => {
+    console.error(error);
+  })
+
 }
 
-// Fetch conversation history using ID from last example
+// flattens all channel history into a single array of messages
 async function fetchHistory(id) {
-  
-  try {
-
-    const channelResults = await bolt.client.conversations.list({token: process.env.SLACK_BOT_TOKEN});
-    const messageHistory = [];
+  return bolt.client.conversations.list({token: process.env.SLACK_BOT_TOKEN})
+  .then(async (channelResults) => {
+    let promiseList = [];
 
     for (let channel of channelResults.channels) {
-      const history = await bolt.client.conversations.history({
+      promiseList.push(bolt.client.conversations.history({
         token: process.env.SLACK_BOT_TOKEN,
         channel: channel.id
-      })
-      for (let message of messageHistory) {
-        message.channel = channel.id;
-      }
-      messageHistory.push(...history.messages);
+      }));
     }
 
-    return messageHistory;
-
-  }
-  catch (error) {
+    return Promise.all(promiseList);
+  })
+  .then((histories) => {
+    messages = [];
+    for (let history of histories) {
+      messages.push(...history.messages)
+    }
+    return messages;
+  })
+  .catch ((error) => {
     console.error(error);
-  }
+  });
 }
 
 module.exports = router;
